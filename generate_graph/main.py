dc711ce9b10f77171ea29d40ab977cd69eb240fb
meetm473 <meetmatehere@gmail.com>
2021-02-18 11:57:21 +0530

    Simulation c17 file

--- generate_graph/main.py
+++ generate_graph/main.py
@@ -10,7 +10,7 @@ import os
 if __name__ == '__main__':
 
     # Relative path 
-    relativePath = '/../test_cases/vm_files/c17.vm'
+    relativePath = '/test_cases/vm_files/c17.vm'
 
     # Absolute file path
     filepath = os.getcwd() + relativePath
@@ -21,3 +21,10 @@ if __name__ == '__main__':
     # Parsing c17.vm
     c17.Parse()
 
+    # simulate
+    print('***Simulation test***')
+    c17.graph.simulate(['N1', 'N2', 'N3', 'N6', 'N7'], '11010')
+    print(10*'-')
+    c17.graph.printPrimeIos(True)
+    print(10*'-')
+    c17.graph.printCfgBlcks(True)
