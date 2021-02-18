2fc57a50770d14eacde7c8134847cc2d0d03d559
vish897 <vishvabhate@gmail.com>
2021-02-18 09:59:12 +0530

    Update parser.py

--- generate_graph/parser.py
+++ generate_graph/parser.py
@@ -28,10 +28,17 @@ with open(filePath, "r") as vmFile:
 lines = len(fData)                      # Number of lines in the file
 lineNo = 0                              # Keeps a track of lineNo being read
 
-inbuf = {}    # Input buffer mapping (Primary Input : wire)
-outbuf = {}   # Output buffer mapping (Primary Output : wire)
+iobuf = {}    # Input buffer mapping (Primary Input : wire)
 CFGio = []
 graph = gu.VerilogGraph() # Graph
+
+def mapIO(iobuf, cfgIO):
+    for key, value in iobuf.items():
+        for j in range(len(cfgIO)):
+            if key == cfgIO[j]:
+                cfgIO[j] = value
+                
+            
 
 while 1:
 
@@ -61,14 +68,14 @@ while 1:
         # Reading the input and output of buffer from next 2 lines
         lineNo += 2
         lineData = re.split('[()]', fData[lineNo-1] + fData[lineNo])
-        inbuf.update({lineData[3]: lineData[1]})
+        iobuf.update({lineData[1]: lineData[3]})
 
     # if the first word of the line is OUTBUF, then store the name of wire and primary output as a pair in outbuf list
     elif lineData[0] == 'OUTBUF':
         # Reading the input and output of buffer from next 2 lines
         lineNo += 2
         lineData = re.split('[()]', fData[lineNo-1] + fData[lineNo])
-        outbuf.update({lineData[1]: lineData[3]})
+        iobuf.update({lineData[3]: lineData[1]})
 
     # if the first word of the line starts with 'CFG', then this is the module used in the program
     elif re.match('^C+F+G', lineData[0]):
@@ -80,6 +87,9 @@ while 1:
             lineNo += 1
         lineNo += 1
         lineData = re.split('[=;\n]', fData[lineNo])
-        graph.addCfgBlck(module, [CFGio[i] for i in range(0, len(CFGio)-1)], CFGio[len(CFGio)-1], lineData[1][4:])
+        mapIO(iobuf, CFGio)
+        graph.addCfgBlck(module, [CFGio[i] for i in range(len(CFGio)-1)], CFGio[len(CFGio)-1], lineData[1][4:])
         CFGio = []
     lineNo += 1
+
+
